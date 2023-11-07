5061fd067492f51753ca93f6756b37e4a3caed4a
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2023-11-07 18:21:34 +0100

    [fix] index

--- db/db.php
+++ db/db.php
@@ -1,14 +1,19 @@
 <?php 
 
 $productions = [
-  new Movie('Blue Beetle','The Safran Company',['Azione','Avventura','Fantascienza','Film Di Supereroi','Thriller'],'DC Studios',new Image('Blue Beetle.jpg','blue beetle'),'2023',127,['Xolo Mariduena','Bruna Marquezine','George Lopez']),
+  new Movie('Blue Beetle','The Safran Company',['Azione','Avventura','Fantascienza','Film Di Supereroi','Thriller'],'DC Studios',
+  new Image('Blue Beetle.jpg','blue beetle'),'2023',127,['Xolo Mariduena','Bruna Marquezine','George Lopez']),
   
-  new TvSerie('Game of Thrones','HBO Entertainment',['fantastico', 'drammatico', 'azione'],'La Compagnia Dorata', new Image('ilTronoDiSpade.jpg','Il Trono Di Spade'),2011,2019,73,8),
+  new TvSerie('Game of Thrones','HBO Entertainment',['fantastico', 'drammatico', 'azione'],'La Compagnia Dorata', 
+  new Image('ilTronoDiSpade.jpg','Il Trono Di Spade'),2011,2019,73,8),
 
-  new Movie('Avatar 2','20th Century Studios',['Azione','Avventura','Fantascienza','Animazione'],'Screen Production Grant',new Image('Avatar.jpg','Avatar2'),'2022',192,['Sam Worthington','Zoe Saldana','Kate Winslet']),
+  new Movie('Avatar 2','20th Century Studios',['Azione','Avventura','Fantascienza','Animazione'],'Screen Production Grant',
+  new Image('Avatar.jpg','Avatar2'),'2022',192,['Sam Worthington','Zoe Saldana','Kate Winslet']),
 
-  new TvSerie('The Walking Dead','AMC Studios Circle of Confusion Darkwood Productions Valhalla Motion Pictures',['orrore', 'postapocalittico', 'drammatico', 'azione'],'AMC', new Image('TheWalkingDead','Il Trono Di Spade'),2010,2022,177,11),
+  new TvSerie('The Walking Dead','AMC Studios Circle of Confusion Darkwood Productions Valhalla Motion Pictures',['orrore', 'postapocalittico', 'drammatico', 'azione'],'AMC', 
+  new Image('TheWalkingDead.jpg','TheWalkingDead'),2010,2022,177,11),
 
-  new Movie('Transformers Rise Of The Beasts','Paramount Pictures',['Azione','Avventura','Fantascienza','Animazione'],'Hasbro',new Image('Avatar.jpg','Avatar2'),'2023',127,['Noah Diaz','Elena Wallace','Arcee']),
+  new Movie('Transformers Rise Of The Beasts','Paramount Pictures',['Azione','Avventura','Fantascienza','Animazione'],'Hasbro',
+  new Image('Trasformers.jpg','Trasformers'),'2023',127,['Noah Diaz','Elena Wallace','Arcee']),
 ]
 ?>
--- index.php
+++ index.php
@@ -6,8 +6,6 @@ require_once __DIR__ . "/models/Movie.php";
 require_once __DIR__ . "/models/TvSerie.php";
 require_once __DIR__ . "/db/db.php";
 
-var_dump($productions);
-
 ?>
 
 
@@ -25,20 +23,33 @@ var_dump($productions);
   <title>Document</title>
 </head>
 <body>
-  <div class="container d-flex my-5">
-    
-  <?php foreach($movies as $movie): ?>
+  <h1 class="text-center my-5">Films & SeriesTV</h1>
+  <div class="container my-5">
+    <div class="row">
 
-    <div class="card m-5" style="width: 18rem;">
-      <img src="img/<?php echo $movie->immagineCopertina->percorsoImmagine ?? 'null.jpg' ?>" class="card-img-top" alt="<?php echo $movie->immagineCopertina->nomeImmagine ?? '' ?>">
-      <div class="card-body">
-        <h5 class="card-title"><?php echo $movie->nome ?></h5>
-        <p class="card-text"><?php echo implode('-',$movie->genere)?></p>
-        <p class="card-text"><?php echo implode('-',$movie->attori)?></p>
-        <p class="card-text"><?php echo $movie->durata ?></p>
-      </div>
+      <?php foreach($productions as $production): ?>
+        <div class="col-auto p-3">
+          <div class="card h-100 m-auto" style="width: 18rem;">
+            <img src="img/<?php echo $production->cover_image->percorsoImmagine ?? 'null.jpg' ?>" class="card-img-top object-fit-cover" style="aspect-ratio: 2/3;" alt="<?php echo $production->cover_image->nomeImmagine ?? '' ?>">
+            <div class="card-body">
+              <h5 class="card-title"><?php echo $production->name ?></h5>
+              <p class="<?= get_class($production) === 'Movie' ? 'badge text-bg-secondary': 'badge text-bg-dark' ?>"><?= get_class($production) ?></p>
+              <p class="card-text"><?php echo implode('-',$production->genre)?></p>
+              
+              <?php if(isset($production->cast) && (get_class($production) == 'Movie')): ?>
+                <p class="card-text"><h6>Cast: <?php echo implode('-',$production->cast)?></h6></p>
+                <p class="card-text"><h6>Durata Film: <?php echo $production->running_time ?></h6></p>
+              <?php else: ?>
+                <p class="card-text"><h6>Anno di produzione: <?php echo $production->aired_from_year ?></h6></p>
+                <p class="card-text"><h6>Anno di fine produzine: <?php echo $production->aired_to_year ?></h6></p>
+                <p class="card-text"><h6>Numero di Episodi Totali: <?php echo $production->number_of_episodes ?></h6></p>
+                <p class="card-text"><h6>Numero di Stagioni: <?php echo $production->number_of_seasons ?></h6></p>
+              <?php endif;?>
+            </div>
+          </div>
+        </div>
+      <?php endforeach; ?>
     </div>
-<?php endforeach; ?>
   </div>
   
 </body>
